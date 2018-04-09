1f4e6df54a794a16810514fe9a18335135ebd6d8
Michael Harrison <harrim91@hotmail.co.uk>
2018-04-09 08:38:15 +0100

    Delete lorem

--- read-file/read-file-sync.js
+++ read-file/read-file-sync.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const filepath = path.join(__dirname, 'hello.txt');
-const lorem = path.join(__dirname, 'lorem.txt');
 
 console.log(1);
 
-const data = fs.readFileSync(lorem, 'utf8');
+const data = fs.readFileSync(filepath, 'utf8');
 
 console.log(data);
